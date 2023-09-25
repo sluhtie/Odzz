75925befab206fc5e514e287456ffe22e984c7c0
Connor W <sluhtie@proton.me>
2023-09-25 14:13:08 +0200

    Initial Commit

---
+++ Odzz/OdzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdzzApp.swift
+//  Odzz
+//
+//  Created by Connor Welge on 25.09.23.
+//
+
+import SwiftUI
+
+@main
+struct OdzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
