29b8c23d141ba6d0e2698e525f83cc9df5e703c4
Connor W <sluhtie@proton.me>
2023-09-28 15:58:21 +0200

    JSON

--- Odzz/JSON/JSONManager.swift
+++ Odzz/JSON/JSONManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+// MARK: - Person
+struct Person: Codable {
+    let firstName, lastName: String
+    let age: Int
+    
+    // Generate samples
+    static let allPeople: [Person] = Bundle.main.decode(file: "students.json")
+    static let samplePerson: Person = allPeople[0]
+}
+
+extension Bundle {
+    func decode<T: Decodable>(file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Could not find \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Could not load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loadedData = try? decoder.decode(T.self, from: data) else {
+            fatalError("Could not decode \(file) from bundle.")
+        }
+        
+        return loadedData
+    }
+}
--- Odzz/Struct/Structs.swift
+++ Odzz/Struct/Structs.swift
@@ -8,9 +8,18 @@
 import Foundation
 import SwiftUI
 
+/// A struct that represents a person.
 struct Student: Identifiable, Hashable, Codable {
+    
+    /// The ID of the person.
     var id: UUID = UUID()
+    
+    /// The first name of the person.
     var firstName: String
+    
+    /// The last name of the person.
     var lastName: String
+    
+    /// The age of the person.
     var age: Int
 }
